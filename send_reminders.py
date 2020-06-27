d4113147a072a05c9ed81373b30910ec0c5689e7
Ashutosh Krishna <ashutoshbritish@gmail.com>
2020-06-28 01:13:29 +0530

    initial commit

---
+++ send_reminders.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+import datetime
+import email
+import smtplib
+import sys
+
+GMAIL_ID = 'email'
+GMAIL_PWD = 'password'
+
+def usage():
+    print("send_reminders: Send meeting reminders")
+    print()
+    print("invocation: ")
+    print("     send_reminders 'date|Meeting Title|Emails' ")
+    return 1
+
+def dow(date):
+    dateobj = datetime.datetime.strptime(date, r"%Y-%m-%d")
+    return dateobj.strftime("%A")
+
+def message_template(date, title):
+    message = email.message.EmailMessage()
+    weekday = dow(date)
+    message['Subject'] = f'Gentle Reminder : "{title}"'
+    message.set_content(f'''
+    Hi there!
+
+    This is a quick mail just to remind you about: "{title}"
+    When : {weekday} | {date}
+
+    See you there.
+    '''
+    )
+    return message
+
+def send_message(message, emails):
+    s = smtplib.SMTP('smtp.gmail.com',587)
+    s.starttls()
+    s.login(GMAIL_ID,GMAIL_PWD)
+    message['From'] = GMAIL_ID
+    for email in emails.split(','):
+        del message['To']
+        message['To'] = email
+        s.send_message(message)
+    s.quit()
+    pass
+
+def main():
+    if len(sys.argv) < 2:
+        return usage()
+
+    try:
+        date, title, emails = sys.argv[1].split('|')
+        message  = message_template(date, title)
+        send_message(message, emails)
+        print("Successfully sent reminders to : ", emails)
+    except Exception as e:
+        print(f"Failed to send email with : {e}", file = sys.stderr)
+
+if __name__ == "__main__":
+    sys.exit(main())
